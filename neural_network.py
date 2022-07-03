3baf77bb1c952fea682a1d054fdd988a6bb7a6fe
Özdeniz Dolu <ozdeniz.dolu@boun.edu.tr>
2022-07-03 23:53:08 +0300

    Refactored neural_network.py, better fnc names

--- neural_network.py
+++ neural_network.py
@@ -41,34 +41,57 @@ class TicTacToe_defaultNN(nn.Module):
         x_eval = self.layer3_2(x)
         return x_moves, x_eval
 
-    # Converts the game state object to input of neural network
-    def state_to_input(self, state):
-        return tr.state_translator(state).to(self._current_device)
+    # Gets a list of game states and returns a batch of valid inputs to network, sent to the same device 
+    def states_to_tensor(self, states):
+        return torch.cat([tr.state_to_tensor(state).unsqueeze(0) for state in states],
+                         dim=0).to(self._current_device)
     
-    # Converts the output of the neural network to be compatible 
-    # with the rest of the system
-    def inv_translate_out(self, state, legal_moves, output):
-        move, ev = output
-        return (
-            tr.inv_move_translator(state, legal_moves, move), 
-            tr.inv_evaluation_translator(ev)
-        )
+    # Converts the move distribution output of the neural network
+    # to be compatible with the rest of the system
+    def tensor_to_dists(self, states, legal_moves_list, moves_tensor):
+        if moves_tensor.ndim != 2:
+            raise ValueError("moves_tensor should have 2 dimensions")
+        if len(states) != len(legal_moves_list):
+            raise ValueError("len(states) should be equal to len(legal_moves_list")
+        if len(states) != moves_tensor.shape[0]:
+            raise ValueError("len(states) should be equal to size of 0th dimension of moves_tensor")
+        return [tr.tensor_to_dist(states[i], legal_moves_list[i], moves_tensor[i])
+                for i in range(len(states))]
+
+    # Converts the evaluation output of the neural network 
+    # to be compatible with the rest of the system
+    def tensor_to_evals(self, evals_tensor):
+        if evals_tensor.ndim != 2:
+            raise ValueError("evals_tensor should have 2 dimensions")
+        return [tr.tensor_to_eval(evals_tensor[i]) for i in range(evals_tensor.shape[0])]
     
-    # Converts the prior probability distributions and evaluations
-    # coming from the system to the same types as the neural network output
-    def translate_out(self, move_distribution, evaluation):
-        return (
-            tr.move_translator(move_distribution), 
-            tr.evaluation_translator(evaluation)
-        )
+    # Converts the list of move_distributions to the same type as 
+    # the neural network output move_distributions
+    def dists_to_tensor(self, move_distributions, device = None):
+        if device == None:
+            device = self._current_device
+        if type(move_distributions) != type([]):
+            raise TypeError("move_distributions should be a list")
+        return torch.cat([tr.dist_to_tensor(move_dist).unsqueeze(0) for move_dist in move_distributions],
+                        dim=0).to(device)
+    
+    # Converts the list of evaluations to the same type as 
+    # the neural network output evaluations
+    def evals_to_tensor(self, evaluations, device = None):
+        if device == None:
+            device = self._current_device
+        if type(evaluations) != type([]):
+            raise TypeError("evaluations should be a list")
+        return torch.cat([tr.eval_to_tensor(evaluation).unsqueeze(0) for evaluation in evaluations],
+                        dim=0).to(device)
     
     # A helper function that is compatible with the evaluator protocol of the system
     def as_evaluator(self):
         def evaluator(state, legal_moves, player):
-            x = self.state_to_input(state).unsqueeze(dim=0)
-            out = self.forward(x)
-            return self.inv_translate_out(state, legal_moves,
-                        map(lambda x: x.squeeze(dim=0), out))
+            x = tr.state_to_tensor(state).unsqueeze(dim=0)
+            move_dist, eval = self.forward(x)
+            return (tr.tensor_to_dist(move_dist.squeeze(0)), 
+                    tr.tensor_to_eval(eval.squeeze(0)))
         return evaluator
 
 
@@ -97,32 +120,55 @@ class TicTacToe_residualNN(nn.Module):
         x_eval = self.layer3_2(x_2)
         return x_moves, x_eval
 
-    # Converts the game state object to input of neural network
-    def state_to_input(self, state):
-        return tr.state_translator(state).to(self._current_device)
+    # Gets a list of game states and returns a batch of valid inputs to network, sent to the same device 
+    def states_to_tensor(self, states):
+        return torch.cat([tr.state_to_tensor(state).unsqueeze(0) for state in states],
+                         dim=0).to(self._current_device)
     
-    # Converts the output of the neural network to be compatible 
-    # with the rest of the system
-    def inv_translate_out(self, state, legal_moves, output):
-        move, ev = output
-        return (
-            tr.inv_move_translator(state, legal_moves, move), 
-            tr.inv_evaluation_translator(ev)
-        )
+    # Converts the move distribution output of the neural network
+    # to be compatible with the rest of the system
+    def tensor_to_dists(self, states, legal_moves_list, moves_tensor):
+        if moves_tensor.ndim != 2:
+            raise ValueError("moves_tensor should have 2 dimensions")
+        if len(states) != len(legal_moves_list):
+            raise ValueError("len(states) should be equal to len(legal_moves_list")
+        if len(states) != moves_tensor.shape[0]:
+            raise ValueError("len(states) should be equal to size of 0th dimension of moves_tensor")
+        return [tr.tensor_to_dist(states[i], legal_moves_list[i], moves_tensor[i])
+                for i in range(len(states))]
+
+    # Converts the evaluation output of the neural network 
+    # to be compatible with the rest of the system
+    def tensor_to_evals(self, evals_tensor):
+        if evals_tensor.ndim != 2:
+            raise ValueError("evals_tensor should have 2 dimensions")
+        return [tr.tensor_to_eval(evals_tensor[i]) for i in range(evals_tensor.shape[0])]
     
-    # Converts the prior probability distributions and evaluations
-    # coming from the system to the same types as the neural network output
-    def translate_out(self, move_distribution, evaluation):
-        return (
-            tr.move_translator(move_distribution), 
-            tr.evaluation_translator(evaluation)
-        )
+    # Converts the list of move_distributions to the same type as 
+    # the neural network output move_distributions
+    def dists_to_tensor(self, move_distributions, device = None):
+        if device == None:
+            device = self._current_device
+        if type(move_distributions) != type([]):
+            raise TypeError("move_distributions should be a list")
+        return torch.cat([tr.dist_to_tensor(move_dist).unsqueeze(0) for move_dist in move_distributions],
+                        dim=0).to(device)
+    
+    # Converts the list of evaluations to the same type as 
+    # the neural network output evaluations
+    def evals_to_tensor(self, evaluations, device = None):
+        if device == None:
+            device = self._current_device
+        if type(evaluations) != type([]):
+            raise TypeError("evaluations should be a list")
+        return torch.cat([tr.eval_to_tensor(evaluation).unsqueeze(0) for evaluation in evaluations],
+                        dim=0).to(device)
     
     # A helper function that is compatible with the evaluator protocol of the system
     def as_evaluator(self):
         def evaluator(state, legal_moves, player):
-            x = self.state_to_input(state).unsqueeze(dim=0)
-            out = self.forward(x)
-            return self.inv_translate_out(state, legal_moves,
-                                          out.squeeze(dim=0))
+            x = tr.state_to_tensor(state).unsqueeze(dim=0)
+            move_dist, eval = self.forward(x)
+            return (tr.tensor_to_dist(move_dist.squeeze(0)), 
+                    tr.tensor_to_eval(eval.squeeze(0)))
         return evaluator
